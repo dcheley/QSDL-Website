fa0b0c3fabd174a312047dd949363b76dc9c7716
Daniel Cheley <danielcheley@hotmail.com>
2017-01-28 19:30:50 -0500

    Creating team params

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,4 +2,10 @@ class TeamsController < ApplicationController
   def index
     @teams = Team.all
   end
+
+  private
+  def team_params
+    params.require(:team).permit(:name, :division, :url)
+  end
+
 end
