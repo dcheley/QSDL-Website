b4a967f3868717fc21eb680dcffae15efa55c354
Daniel Cheley <danielcheley@hotmail.com>
2017-01-28 19:34:43 -0500

    Starting seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+teams = team.create([{ name: 'Arrows', division: 'B1', url: }, {}, {}])
