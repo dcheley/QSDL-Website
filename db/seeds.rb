e6f0ae998ba9f878eed01f96527cfdcc2bff7133
Daniel Cheley <danielcheley@hotmail.com>
2017-01-29 14:31:50 -0500

    Completing team seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-teams = team.create([{ name: 'Arrows', division: 'B1', url: }, {}, {}])
+Team.create([{ name: 'Gravediggers', division: 'A', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=728" },
+  { name: 'Dart Bags 1', division: 'A', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=434" },
+  { name: 'Nomads', division: 'A', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=37" },
+  { name: 'A Nightmare on Elm Street', division: 'A', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=699" },
+  { name: 'Tungsten Ticklers', division: 'A', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=49" },
+  { name: 'Harp Devils', division: 'A', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=764" },
+  { name: 'G-Men', division: 'A', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=432" },
+  { name: 'Bullseyes', division: 'A', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=32" },
+
+  { name: 'Dart Bags 2', division: 'B1', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=313" },
+  { name: 'Screaming Eagles', division: 'B1', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=789" },
+  { name: 'Anger Management', division: 'B1', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=24" },
+  { name: 'Balrogs', division: 'B1', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=51" },
+  { name: 'All Blacks', division: 'B1', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=39" },
+  { name: 'Arrows', division: 'B1', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=741" },
+  { name: 'DWA', division: 'B1', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=793" },
+  { name: 'DCMC', division: 'B1', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=790" },
+
+  { name: "McMurphy's", division: 'B2', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=701" },
+  { name: 'Bored Members', division: 'B2', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=766" },
+  { name: 'Throwers', division: 'B2', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=41" },
+  { name: 'Dart Demons', division: 'B2', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=700" },
+  { name: 'Rogues and Rovers', division: 'B2', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=791" },
+  { name: 'Greeks', division: 'B2', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=23" },
+  { name: "Barbed Wire's", division: 'B2', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=792" },
+  { name: 'Dart Vaders', division: 'B2', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=26" },
+
+  { name: 'C Eagles', division: 'C', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=312" },
+  { name: "Maysie's Crazies", division: 'C', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=673" },
+  { name: 'Black Rats', division: 'C', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=31" },
+  { name: 'Rails and Ales', division: 'C', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=29" },
+  { name: 'Dart Tarts', division: 'C', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=103" },
+  { name: 'The Focus Group', division: 'C', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=788" }])
