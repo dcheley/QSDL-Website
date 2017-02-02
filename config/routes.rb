2e208ead97efa8fb59627f3976f019ae53b9a1fa
Daniel Cheley <danielcheley@hotmail.com>
2017-02-02 02:38:55 -0500

    Adding sessions post route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "pages#show", page: "home"
 
   resources :users, only: [:edit]
-  resources :sessions, only: [:new, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :teams, only: [:index, :edit]
   resources :bars, only: [:index, :edit]
 
