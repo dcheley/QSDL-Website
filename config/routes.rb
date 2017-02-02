4d5e5794528d69385b844288eb4a72d0b58797a6
Daniel Cheley <danielcheley@hotmail.com>
2017-02-02 04:06:25 -0500

    Adding new and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root "pages#show", page: "home"
 
   resources :users, only: [:edit, :update]
-  resources :teams, only: [:index, :edit, :update]
-  resources :bars, only: [:index, :edit, :update]
+  resources :teams, only: [:index, :edit, :update, :new, :create]
+  resources :bars, only: [:index, :edit, :update, :new, :create]
 
   get "/pages/*page" => "pages#show"
   get 'login' => 'sessions#new', :as => :login
