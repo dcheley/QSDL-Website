c28d97389e56d79cb2c28d529aa2cbe7711b6e3e
Daniel Cheley <danielcheley@hotmail.com>
2017-02-02 04:05:59 -0500

    Adding validations

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,3 +1,5 @@
 class Bar < ApplicationRecord
   belongs_to :user
+
+  validates :name, :address, :phone, :url, presence: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,5 @@
 class Team < ApplicationRecord
   belongs_to :user
+
+  validates :name, :division, :url, presence: true
 end
