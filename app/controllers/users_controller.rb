e03e6cf311ed071444b52a4d9a696a4247e9b1da
Daniel Cheley <danielcheley@hotmail.com>
2017-02-02 02:04:28 -0500

    Creating controller, edit method and user params

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def edit
+    @user = User.find_by(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
