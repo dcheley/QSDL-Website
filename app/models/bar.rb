6ad8f353e3d11a5913fadd624d20a48c830881f3
Daniel Cheley <danielcheley@hotmail.com>
2017-02-02 01:38:57 -0500

    Creating user table and model relations

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,2 +1,3 @@
 class Bar < ApplicationRecord
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128060557) do
+ActiveRecord::Schema.define(version: 20170202063522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20170128060557) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "division"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
