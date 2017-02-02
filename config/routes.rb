b2f73c1ff5187ea9a36b70702524d61fe0616fc0
Daniel Cheley <danielcheley@hotmail.com>
2017-02-02 01:51:39 -0500

    adding sessions and user routes, adding login/logout links to footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "pages#show", page: "home"
 
+  resources :users, only: [:edit]
+  resources :sessions, only: [:new, :destroy]
   get "/pages/*page" => "pages#show"
   get 'teams/index'
   get 'bars/index'
