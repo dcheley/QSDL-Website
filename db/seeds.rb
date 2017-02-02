45b8ccb3eb9c787620102043c6d309ff4aeec1df
Daniel Cheley <danielcheley@hotmail.com>
2017-02-02 01:56:07 -0500

    Seeding test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+User.create([{ email: 'test', password: [testing!]}])
+
 Team.create([{ name: 'Gravediggers', division: 'A', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=728" },
   { name: 'Dart Bags 1', division: 'A', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=434" },
   { name: 'Nomads', division: 'A', url: "http://www.yourleaguestats.com/darts/qsdl/team.php?id=37" },
