bef4e5a2f7433f5818cde1dc953e70fe19695b58
Daniel Cheley <danielcheley@hotmail.com>
2017-02-04 17:11:42 -0500

    Implementing news controller CRUD logic

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,17 +1,34 @@
 class NewsController < ApplicationController
   def new
+    @news = News.new
   end
 
   def create
+    @news = News.new(news_params)
+    if @news.save
+      redirect_to "/news/index", notice: "Article added!"
+    else
+      render :new
+    end
   end
 
   def edit
+    @news = News.find(params[:id])
   end
 
   def update
+    @news = News.find(params[:id])
+    if @news.update_attributes(news_params)
+      redirect_to "/news/index", notice: "Article updated!"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @news = News.find(params[:id])
+    @news.destroy
+    redirect_to "/news/index", notice: "Article deleted!"
   end
 
   def index
