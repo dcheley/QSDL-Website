d16a4793b2aecd388eeb87bd5969d76671299ec4
dcheley <danielcheley@hotmail.com>
2018-02-06 15:28:18 -0500

    Indenting seeds, setting foreign key to optional for testing and converting team drop downs into lists

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
-  belongs_to :bar
+  belongs_to :bar, optional: true
 
   validates :name, :division, :url, presence: true
 end
