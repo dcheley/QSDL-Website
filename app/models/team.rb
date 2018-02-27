05098665462135addcb69706c403953e55c75dad
dcheley <danielcheley@hotmail.com>
2018-02-27 09:38:22 -0500

    Using enum for team divisions

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,8 @@
 class Team < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
+  enum division: { Premier: 0, A:, 1, B_East: 2, B_West: 3, C: 4 } 
+
   belongs_to :bar
 
   validates :name, :division, :url, presence: true
