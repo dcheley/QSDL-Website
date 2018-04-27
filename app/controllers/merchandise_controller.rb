9ba0b671a75a5c052a390c33b06ff777be183bd2
dcheley <danielcheley@hotmail.com>
2018-04-27 11:51:25 -0400

    Adding admin route and implementing controller logic

--- app/controllers/merchandise_controller.rb
+++ app/controllers/merchandise_controller.rb
@@ -1,25 +1,47 @@
 class MerchandiseController < ApplicationController
   def new
+    @merchandise = Merchandise.new
   end
 
   def create
+    @merchandise = Merchandise.new(merchandise_params)
+    if @merchandise.save
+      redirect_to :merchandise_list, notice: "#{@merchandise.name} created"
+    else
+      render :new
+    end
   end
 
   def edit
+    @merchandise = Merchandise.find(params[:id])
   end
 
   def update
+    @merchandise = Merchandise.find(params[:id])
+    if @merchandise.update_attributes(merchandise_params)
+      redirect_to :merchandise_list, notice: "#{@merchandise.name} updated"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @merchandise = Merchandise.find(params[:id])
+    @merchandise.destroy
+    redirect_to :merchandise_list, notice: "#{@merchandise.name} deleted"
   end
 
   def index
+    @merchandise = Merchandise.all.order("updated_at DESC")
+  end
+
+  def merchandise_list
+    @merchandise = Merchandise.all.order("updated_at DESC")
   end
 
   private
 
   def merchandise_params
-    params.require(:merchandise).permit(:avatar)
+    params.require(:merchandise).permit(:name, :description, :price, :avatar)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "/teams/team_list" => "teams#team_list", as: :team_list
   get "/bars/bar_list" => "bars#bar_list", as: :bar_list
   get "/news/news_list" => "news#news_list", as: :news_list
+  get "/merchandise/merchandise_list" => "merchandise#merchandise_list", as: :merchandise_list
   get "/memorials/memorium_list" => "memorials#memorium_list", as: :memorium_list
   get "/news/captains" => "news#captains", as: :captain_news
   get "/news/players" => "news#players", as: :player_news
