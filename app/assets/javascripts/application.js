7fa4cf1c70d0b733edb92b2f175c8037ead8f6df
dcheley <danielcheley@hotmail.com>
2018-09-05 18:30:01 -0400

    Removing extra DOM event

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,32 +2,29 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require materialize
+$(document).on('turbolinks:load', function() {
+  $('.button-collapse').sideNav();
+  $('.parallax').parallax();
+  $('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the right of button
+    stopPropagation: false // Stops event propagation
+  });
+  $('.modal').modal();
+  $('.fixed-action-btn').hide();
+  $(window).scroll(function() {
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = 50;
 
-$(document).ready(function() {
-  $(document).on('turbolinks:load', function() {
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the right of button
-      stopPropagation: false // Stops event propagation
-    });
-    $('.modal').modal();
-    $('.fixed-action-btn').hide();
-    $(window).scroll(function() {
-      var y_scroll_pos = window.pageYOffset;
-      var scroll_pos_test = 50;
-
-      if (y_scroll_pos > scroll_pos_test) {
-        $(".fixed-action-btn").show();
-      } else {
-        $(".fixed-action-btn").hide();
-      }
-    });
+    if (y_scroll_pos > scroll_pos_test) {
+      $(".fixed-action-btn").show();
+    } else {
+      $(".fixed-action-btn").hide();
+    }
   });
 });
